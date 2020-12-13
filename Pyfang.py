d3b54b66a49667fe2ad4d5eae42e33bcc40979c6
Pyfang <2950966704@qq.com>
2020-12-13 16:17:36 +0800

    Add an update

--- Pyfang.py
+++ Pyfang.py
@@ -1,6 +1,6 @@
-import time
-import os
+import easygui as es
 import turtle as tu
+import tkinter as tk
 bl = []
 bls = []
 def valuecheck(checks):
@@ -42,41 +42,58 @@ def pyfangrun(command):
             else:
                 bl.append(splited[0])
                 bls.append(valuecheck(splited[2]))
-        elif splited[0] == 'print' and len(splited) < 3:
-            print(valuecheck(splited[1]))
         elif splited[0] == 'print':
-            print(valuecheck(splited[1]))
+            t1.color('green')
+            t1.write(valuecheck(splited[1]),font=('arial',12))
+            t1.setheading(-90)
+            t1.penup()
+            t1.fd(24)
         elif splited[0] == 'fd':
-            tu.fd(int(splited[1]))
+            t2.fd(int(splited[1]))
         elif splited[0] == 'back':
-            tu.back(int(splited[1]))
+            t2.back(int(splited[1]))
         elif splited[0] == 'head':
-            tu.setheading(int(splited[1]))
+            t2.setheading(int(splited[1]))
         elif splited[0] == 'color':
-            tu.pencolor(splited[1])
+            t2.pencolor(splited[1])
         elif splited[0] == 'dot':
-            tu.dot(int(splited[1]))
+            t2.dot(int(splited[1]))
         elif splited[0] == 'size':
-            tu.pensize(int(splited[1]))
+            t2.pensize(int(splited[1]))
         elif splited[0] == 'right':
-            tu.right(int(splited[1]))
+            t2.right(int(splited[1]))
         elif splited[0] == 'left':
-            tu.left(int(splited[1]))
+            t2.left(int(splited[1]))
         elif splited[0] == 'goto':
-            tu.goto(int(splited[1]),int(splited[2]))
+            t2.goto(int(splited[1]),int(splited[2]))
         elif splited[0] == 'msgbox':
             es.msgbox(valuecheck(splited[1]),valuecheck(splited[2]),valuecheck(splited[3]))
         else:
             es.msgbox("NameError: name "+splited[0]+"' is not defined",'Error')
     elif splited[0] == 'down':
-        tu.pendown()
+        t2.pendown()
     elif splited[0] == 'up':
-        tu.penup()
+        t2.penup()
     elif splited[0] == 'clear':
-        tu.clear()
+        t2.clear()
     else:
         es.msgbox("NameError: name "+splited[0]+"' is not defined",'Error')
-print('''
+r = tu._Root()
+r.set_geometry(800,800,0,0)
+cv2 = tu.ScrolledCanvas(r)
+cv2.pack(expand = True, fill = tk.BOTH)
+cv2.reset(400, 4000)
+t1 = tu.RawPen(cv2)
+cv3 = tu.ScrolledCanvas(r)
+cv3.pack(expand = True, fill = tk.BOTH)
+cv3.reset(400, 400)
+t2 = tu.RawPen(cv3)
+t2.shape('turtle')
+t1.hideturtle()
+t1.penup()
+t1.goto(-350,70)
+t1.color('green')
+t1.write('''
   _____        __                  
  |  __ \      / _|                 
  | |__) |   _| |_ __ _ _ __   __ _ 
@@ -85,7 +102,11 @@ print('''
  |_|    \__, |_| \__,_|_| |_|\__, |
          __/ |                __/ |
         |___/                |___/ 
-''')
+''',font=('arial',12))
+t1.setheading(-90)
+t1.penup()
+t1.fd(24)
+t1.color('purple')
 f = open('file.pfr','r',encoding='utf-8')
 wh = open('while.con','r',encoding='utf-8')
 if wh.readlines()[0] == 'True':
